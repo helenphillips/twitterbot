1a8de6e9bedb0ad9aa614f4db019a89acc72cb80
Helen Phillips <helen.r.phillips@googlemail.com>
2013-08-11 15:07:57 +0100

    adding random choice of multiple statuses

--- dinobot.py
+++ dinobot.py
@@ -1,15 +1,20 @@
-import twitter, os, time
+import twitter, os, time, sys, random
+from random import randint
+
 
 api=twitter.Api(consumer_key ='',
 consumer_secret='',
 access_token_key= '',
 access_token_secret='')
 
+
 LATESTFILE = 'dinosays_latest.txt'
 LOGFILE = 'dinosays_log.txt'
 
 os.chdir('/Users/Helen/twitter')
 
+# possible replies
+statuses = (' roarrrr!', ' RRROOOOAAAARRR!!!!', ' roar!', ' ROAR!',' RRRRRRRooooooaaaaaarrrr!!!!!', ' meow!', ' ROAR ROAR RRROOOOARRR!',' ROARRRR!')
 
 # grab the last ID that the bot replied to, so it doesn't reply to earlier posts. (spam prevention measure)
 if os.path.exists(LATESTFILE):
@@ -34,12 +39,16 @@ for i in range(len(alreadyMessaged)):
 alreadyMessaged.append('DinosSayRoar') # don't reply to myself
 
 # perform the search
-results = api.GetSearch('dinosaurs extinct wish', since_id=lastid)
-#print 'Found %s results.' % (len(results))
+results = api.GetSearch('dinosaurs', since_id=lastid)
+print 'Found %s results.' % (len(results))
 if len(results) == 0:
-    #print 'Nothing to reply to. Quitting.'
+    print 'Nothing to reply to. Quitting.'
     sys.exit()
 repliedTo = []
+if len(results) > (len(statuses) * len(ment)):
+    results = random.sample(results, (len(statuses) * len(ment)))
+
+
 
 for statusObj in results:
     statusObj.created_at = statusObj.created_at[:-11] + statusObj.created_at[25:]
@@ -47,12 +56,12 @@ for statusObj in results:
 
     if time.time() - (24*60*60) < postTime and statusObj.user.screen_name not in alreadyMessaged and '@DinosSayRoar' not in statusObj.text.lower():
         if [True for x in alreadyMessaged if ('@' + x).lower() in statusObj.text.lower()]:
-            #print 'Skipping because it\'s a mention: @%s - %s' % (statusObj.user.screen_name.encode('ascii', 'replace'), statusObj.text.encode('ascii', 'replace'))
+            print 'Skipping because it\'s a mention: @%s - %s' % (statusObj.user.screen_name.encode('ascii', 'replace'), statusObj.text.encode('ascii', 'replace'))
             continue
 
         try:
-            #print 'Posting in reply to @%s: %s' % (statusObj.user.screen_name.encode('ascii', 'replace'), statusObj.text.encode('ascii', 'replace'))
-            api.PostUpdate('@%s ROARRRR!' % (statusObj.user.screen_name), in_reply_to_status_id=statusObj.id)
+            print 'Posting in reply to @%s: %s' % (statusObj.user.screen_name.encode('ascii', 'replace'), statusObj.text.encode('ascii', 'replace'))
+            api.PostUpdate('@%s' % (statusObj.user.screen_name) + statuses[randint(1, len(statuses))] , in_reply_to_status_id=statusObj.id)
             repliedTo.append( (statusObj.id, statusObj.user.screen_name, statusObj.text.encode('ascii', 'replace')) )
             time.sleep(1)
         except Exception:
